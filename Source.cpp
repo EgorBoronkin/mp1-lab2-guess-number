683585ff74c5830598a773cdbe414b26576e73cc
Boronkin Egor <113104407+EgorBoronkin@users.noreply.github.com>
2022-10-11 10:14:52 +0300

    fix

--- Source.cpp
+++ Source.cpp
@@ -1,33 +1,84 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 void main()
 {
-	int h, w, d;
-	float  p_dvp, p_dsp, p_wood, weight;
-	p_dsp = 0.6;
-	p_dvp = 0.8;
-	p_wood = 0.5;
-	printf("Density of dsp - %f g/cm cubed, density of dvp - %f g/cm cubed, density of wood - %f g/cm cubed \n", p_dsp, p_dvp, p_wood);
-	printf("Write height from 180 cm to 220 cm:");
-	scanf_s("%i", &h);
-	printf("Write height from 80 cm to 120 cm:");
-	scanf_s("%i", &w);
-	printf("Write height from 50 cm to 90 cm:");
-	scanf_s("%i", &d);
-	if ((180 <= h && h <= 220) && (80 <= w && w <= 120) && (50 <= d && d <= 90))
+	srand(time(NULL));
+	int modes, random, user, count, answer_for_pc, max, min;
+	printf("Program have 2 modes \n");
+	printf("First mode : program have a number from 1 to 1000, user need guess this number, program write <, >, or = \n");
+	printf("When user choose right number - program complete \n");
+	printf("Second: User have a number from 1 to 1000, program try guess, user write <, >, or = \n");
+	printf("When program choose right number - program complete \n");
+	printf("Choose first or second mode: ");
+	scanf_s("%i", &modes);
+	while (2 < modes || modes == 0)
 	{
-		if (h < 200)
+		printf("Wrong mode. Try to choose your mode one more time: ");
+		scanf_s("%i", &modes);
+	}
+	if (modes == 1)
+	{
+		random = 1 + rand() % (1000 - 1 + 1);
+		printf("%i \n", random);
+		count = 1;
+		printf("First program \n");
+		printf("Guess a number: ");
+		scanf_s("%i", &user);
+		while (user != random)
 		{
-			weight = ((h * w * 0.5 * p_dvp) + (h * d * 1.5 * p_dsp * 2) + (w * d * 1.5 * p_dsp * 2) + (h * w * p_wood * 2) + (w * d * 1.5 * p_dsp * 4)) / 1000;
-			printf("Weight of wardrobe - %.2f kg", weight);
+			if (user < random)
+			{
+				printf("> \n");
+				printf("Guess a number one more time: ");
+				scanf_s("%i", &user);
+				count += 1;
+			}
+			else if (user > random)
+			{
+				printf("< \n");
+				printf("Guess a number one more time: ");
+				scanf_s("%i", &user);
+				count += 1;
+			}
 		}
-		else
-		{
-			weight = ((h * w * 0.5 * p_dvp) + (h * d * 1.5 * p_dsp * 2) + (w * d * 1.5 * p_dsp * 2) + (h * w * p_wood * 2) + (w * d * 1.5 * p_dsp * 5)) / 1000;
-			printf("Weight of wardrobe - %.2f kg", weight);
-		}
+		printf("= \n");
+		printf("Number of attempts - %i", count);
 	}
 	else
 	{
-		printf("Wrong data");
+		printf("Second program(Info for user. 1 - <, 2 - >, 3 - =) \n");
+		printf("Write a number: \n");
+		scanf_s("%i", &user);
+		random = 1 + rand() % (1000 - 1 + 1);
+		count = 1;
+		max = 1000;
+		min = 1;
+		printf("Computer: is it %i? \n", random);
+		printf("Answer for computer: ");
+		scanf_s("%i", &answer_for_pc);
+		while (answer_for_pc != 3)
+		{
+			if (answer_for_pc == 1)
+			{
+				max = random - 1;
+				random = min + rand() % (max - min + 1);
+				count += 1;
+				printf("Computer: is it %i? \n", random);
+				printf("Answer for computer: ");
+				scanf_s("%i", &answer_for_pc);
+			}
+			if (answer_for_pc == 2)
+			{
+				min = random + 1;
+				random = min + rand() % (max - min + 1);
+				count += 1;
+				printf("Computer: is it %i? \n", random);
+				printf("Answer for computer: ");
+				scanf_s("%i", &answer_for_pc);
+			}
+		}
+		printf("= \n");
+		printf("Number of attempts - %i", count);
 	}
 }
